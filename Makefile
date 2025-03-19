2a9669de100783f38efec973d0b0742949e4a426
npmthu <npmthu23@clc.fitus.edu.vn>
2025-03-19 12:28:04 +0000

    sysinfo

--- Makefile
+++ Makefile
@@ -194,6 +194,10 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
+	$U/_pingpong\
+	$U/_find\
+	$U/_primes\
+	$U/_xargs\
 	$U/_sysinfotest\
 
 
--- user/sysinfo.c
+++ user/sysinfo.c
@@ -1,306 +1,200 @@
-
 #include "kernel/types.h"
-
 #include "kernel/riscv.h"
-
 #include "kernel/sysinfo.h"
-
 #include "user/user.h"
 
 
-
-
-
 void
-
 sinfo(struct sysinfo *info) {
-
   if (sysinfo(info) < 0) {
-
     printf("FAIL: sysinfo failed");
-
     exit(1);
-
   }
-
 }
 
-
-
 //
-
 // use sbrk() to count how many free physical memory pages there are.
-
 //
-
 int
-
 countfree()
-
 {
-
   uint64 sz0 = (uint64)sbrk(0);
-
   struct sysinfo info;
-
   int n = 0;
 
-
-
   while(1){
-
     if((uint64)sbrk(PGSIZE) == 0xffffffffffffffff){
-
       break;
-
     }
-
     n += PGSIZE;
-
   }
-
   sinfo(&info);
-
   if (info.freemem != 0) {
-
     printf("FAIL: there is no free mem, but sysinfo.freemem=%ld\n",
-
       info.freemem);
-
     exit(1);
-
   }
-
   sbrk(-((uint64)sbrk(0) - sz0));
-
   return n;
-
 }
 
-
-
 void
-
 testmem() {
-
   struct sysinfo info;
-
   uint64 n = countfree();
-
   
-
   sinfo(&info);
-
-
 
   if (info.freemem!= n) {
-
     printf("FAIL: free mem %ld (bytes) instead of %ld\n", info.freemem, n);
-
     exit(1);
-
   }
-
   
-
   if((uint64)sbrk(PGSIZE) == 0xffffffffffffffff){
-
     printf("sbrk failed");
-
     exit(1);
-
   }
 
-
-
   sinfo(&info);
-
     
-
   if (info.freemem != n-PGSIZE) {
-
     printf("FAIL: free mem %ld (bytes) instead of %ld\n", n-PGSIZE, info.freemem);
-
     exit(1);
-
   }
-
   
-
   if((uint64)sbrk(-PGSIZE) == 0xffffffffffffffff){
-
     printf("sbrk failed");
-
     exit(1);
-
   }
-
-
 
   sinfo(&info);
-
     
-
   if (info.freemem != n) {
-
     printf("FAIL: free mem %ld (bytes) instead of %ld\n", n, info.freemem);
-
     exit(1);
-
   }
-
 }
-
-
 
 void
-
 testcall() {
-
   struct sysinfo info;
-
   
-
   if (sysinfo(&info) < 0) {
-
     printf("FAIL: sysinfo failed\n");
-
     exit(1);
-
   }
-
-
 
   if (sysinfo((struct sysinfo *) 0xeaeb0b5b00002f5e) !=  0xffffffffffffffff) {
-
     printf("FAIL: sysinfo succeeded with bad argument\n");
-
     exit(1);
-
   }
-
 }
-
-
 
 void testproc() {
-
   struct sysinfo info;
-
   uint64 nproc;
-
   int status;
-
   int pid;
-
   
-
   sinfo(&info);
-
   nproc = info.nproc;
 
-
-
   pid = fork();
-
   if(pid < 0){
-
     printf("sysinfotest: fork failed\n");
-
     exit(1);
-
   }
-
   if(pid == 0){
-
     sinfo(&info);
-
     if(info.nproc != nproc+1) {
-
       printf("sysinfotest: FAIL nproc is %ld instead of %ld\n", info.nproc, nproc+1);
-
       exit(1);
-
     }
-
     exit(0);
-
   }
-
   wait(&status);
-
   sinfo(&info);
-
   if(info.nproc != nproc) {
-
       printf("sysinfotest: FAIL nproc is %ld instead of %ld\n", info.nproc, nproc);
-
       exit(1);
-
   }
-
 }
-
-
 
 void testbad() {
+  int pid = fork();
+  int xstatus;
+  
+  if(pid < 0){
+    printf("sysinfotest: fork failed\n");
+    exit(1);
+  }
+  if(pid == 0){
+      sinfo(0x0);
+      exit(0);
+  }
+  wait(&xstatus);
+  if(xstatus == -1)  // kernel killed child?
+    exit(0);
+  else {
+    printf("sysinfotest: testbad succeeded %d\n", xstatus);
+    exit(xstatus);
+  }
+}
+void testloadavg() {
+  struct sysinfo info;
+  uint64 baseline, new_loadavg;
+  int status;
+  
+  sinfo(&info);
+  baseline = info.loadavg;
 
   int pid = fork();
-
-  int xstatus;
-
-  
-
-  if(pid < 0){
-
-    printf("sysinfotest: fork failed\n");
-
-    exit(1);
-
+  if (pid < 0) {
+      printf("sysinfotest: fork failed\n");
+      exit(1);
   }
 
-  if(pid == 0){
-
-      sinfo(0x0);
-
+  if (pid == 0) {
+      // Child process: chạy vòng lặp để giữ CPU bận
+      for (volatile int i = 0; i < 100000000; i++);
       exit(0);
-
   }
 
-  wait(&xstatus);
+  // Parent process: kiểm tra `loadavg`
+  sinfo(&info);
+  new_loadavg = info.loadavg;
 
-  if(xstatus == -1)  // kernel killed child?
-
-    exit(0);
-
-  else {
-
-    printf("sysinfotest: testbad succeeded %d\n", xstatus);
-
-    exit(xstatus);
-
+  if (new_loadavg <= baseline) {
+      printf("sysinfotest: FAIL loadavg did not increase (was %ld, now %ld)\n", baseline, new_loadavg);
+      exit(1);
   }
 
+  wait(&status); // Đợi child process kết thúc
+
+  sinfo(&info);
+  if (info.loadavg != baseline) {
+      printf("sysinfotest: FAIL loadavg did not return to baseline (was %ld, now %ld)\n", baseline, info.loadavg);
+      exit(1);
+  }
 }
 
-
-
 int
-
 main(int argc, char *argv[])
-
 {
-
   printf("sysinfotest: start\n");
-
   testcall();
-
   testmem();
-
   testproc();
-
-  printf("sysinfotest: OK\n");
-
+  testloadavg();
+  
+  struct sysinfo info;
+  if (sysinfo(&info) < 0) {
+    printf("FAIL: sysinfo failed\n");
+    exit(1);
+  }
+  printf("Free memory: %ld\n", info.freemem);
+  printf("Number of processes: %ld\n", info.nproc);
+  printf("Load average: %ld\n", info.loadavg);
+  
   exit(0);
-
 }
